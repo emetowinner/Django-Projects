2bcfa9a82d67dd19d92a9354f0a3031df2eb7437
emetowinner <emetowinner@gmail.com>
2021-02-17 22:39:35 +0100

    coplete pet adoption project

--- viccy/adoptions/views.py
+++ viccy/adoptions/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Pet
 
-# Create your views here.
+def home(request):
+    pets = Pet.objects.all()
+    context = {'pets':pets}
+    return render(request,'home.html',context)
+
+def pet_detail(requst,pet_id):
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found!')
+    context = {'pet':pet}
+    return render(requst,'pet_detail.html',context)
